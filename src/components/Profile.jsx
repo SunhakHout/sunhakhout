e0938cf6d7047c97308b645f330915fc2c1b4f08
Sunhak <sunhak.class@gmail.com>
2018-04-25 00:17:35 +0700

    profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+export default class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <button className="btn btn-success"> Testing Bootstrap 3 </button>
+      </Fragment>
+    )
+  }
+}
